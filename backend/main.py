180e3ee5caaacbb13e54d55a82b5f2a02c7ec04c
Vidhi Doshi <69749658+Asubtlecoderrr@users.noreply.github.com>
2025-04-25 00:18:08 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://34.30.100.226:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
