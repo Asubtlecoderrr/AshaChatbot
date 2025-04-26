b3629941662126bd007a40be64c1ef3d61ae8285
Asubtlecoderrr <vidhidoshi039@gmail.com>
2025-04-27 03:40:14 +0530

    added skills and location var

--- shared/user_context.py
+++ shared/user_context.py
@@ -3,3 +3,7 @@ import contextvars
 # Create a context variable for user_id
 user_id_var = contextvars.ContextVar("user_id")
 user_id_var.set(None)
+skill_var = contextvars.ContextVar("skill")
+skill_var.set(None)
+location_var = contextvars.ContextVar("location")
+location_var.set(None)
